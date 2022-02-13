927d05cbb119f162b1e1fa302f483bc912747c4b
miteshpr97 <miteshpradhan97@gmail.com>
2022-02-13 19:59:39 +0530

    Updates

---
+++ precache-manifest.9ccfb0226f6d187145a0d7b5fed45862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d109436840d294d70bc",
+    "url": "/React-assignment/static/css/main.f49d2de2.chunk.css"
+  },
+  {
+    "revision": "6d109436840d294d70bc",
+    "url": "/React-assignment/static/js/main.9eb8911e.chunk.js"
+  },
+  {
+    "revision": "0dc9ce39af308526980a",
+    "url": "/React-assignment/static/js/runtime~main.1b363dd3.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/React-assignment/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "8fd3f86080363b44c6051c5bfea37d7c",
+    "url": "/React-assignment/index.html"
+  }
+];
